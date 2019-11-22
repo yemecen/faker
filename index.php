bc331ded0a5f9c55314b92bd3dbd18893c63f2f8
yemecen <y.emecen@gmail.com>
2019-11-23 01:14:29 +0300

    First Demo

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php 
 
+require_once 'vendor/autoload.php';
+
+$faker = Faker\Factory::create('tr_TR');
+
+$array = [
+   "Adres" => $faker->address,
+   "Tc No" => $faker->tcNo,
+   "Telefon" => $faker->phoneNumber,
+   "Slug" => $faker->slug,
+   "MAC Adresi" => $faker->macAddress,
+   "Tarih" => date($format = 'Y-m-d', $max = 'now')
+];
+
+print_r("<pre>".$array."</pre>");
+
 ?>
